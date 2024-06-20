acdaac766306bc56355c49b6129c3aa7151b73d0
DamianStr02 <damian00str@gmail.com>
2024-06-20 10:52:42 +0200

    Update process_comment.php

--- process_comment.php
+++ process_comment.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $review = htmlspecialchars($_POST['review']);
     $name = htmlspecialchars($_POST['name']);
